f6a48014e924ea0cc83aceae9d54aa392f31b650
alexleonvader <alexleonvader@github.com>
2015-06-02 17:19:47 -0500

    2 Junio

---
+++ src/Sisaf/SisafBundle/Form/GastosFijosType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sisaf\SisafBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GastosFijosType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Nombre')
+            ->add('Descripcion')
+            ->add('Monto')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sisaf\SisafBundle\Entity\GastosFijos'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'sisaf_sisafbundle_gastosfijostype';
+    }
+}
