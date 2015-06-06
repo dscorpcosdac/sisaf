f4f892706e5d105a35194a61e064ee1856948cab
AlexLeonVader <alexleonvader@github.com>
2015-06-06 00:16:07 -0500

    Login desde Base de Datos

--- src/Sisaf/SisafBundle/Controller/SecurityController.php
+++ src/Sisaf/SisafBundle/Controller/SecurityController.php
@@ -18,11 +18,11 @@ class SecurityController extends Controller
             $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
         } else {
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            //$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
         }
          $em = $this->getDoctrine()->getManager();
-         $entity = new usuario();
-         $form   = $this->CreateFormUser($entity);
+         //$entity = new usuario();
+         //$form   = $this->CreateFormUser($entity);
 
 
 
@@ -30,31 +30,13 @@ class SecurityController extends Controller
                 array(
                     // last username entered by the user
                     'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                    'form'   => $form->createView(),'id'=>0,
+                    //'form'   => $form->createView(),'id'=>0,
                     'error'         => $error,
-                    'directorio'  => $template->getRuta(),
+                    //'directorio'  => $template->getRuta(),
                 )
             );
     }
 
-    /**
-     * Creates a form to create a usuario entity.
-     *
-     * @param usuario $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function CreateFormUser(usuario $entity)
-    {
-        $form = $this->createForm(new usuarioType(), $entity, array(
-            'action' => $this->generateUrl('inicio'),
-            'method' => 'POST',
-        ));
-
-        $form->add('Guardar', 'submit', array('label'=>'','attr' => array('class' => 'btn btn-primary')));
-
-        return $form;
-    }
 }
 
 
---
+++ src/Sisaf/SisafBundle/Entity/Usuario.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Sisaf\SisafBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="Usuario")
+ * @ORM\Entity(repositoryClass="SisafBundle\Entity\UsuarioRepository")
+ */
+
+/**
+ * Usuario
+ */
+class Usuario implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        // may not be needed, see section on salt below
+        // $this->salt = md5(uniqid(null, true));
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Usuario
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Usuario
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_ADMIN');
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized);
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Usuario
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return Usuario
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
